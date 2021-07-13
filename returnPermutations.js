b614840f5caf43cafaa0867a6172b4ebc548fbb6
Harrison Gerdes <harrisongerdes@gmail.com>
2021-07-13 18:32:14 -0400

    style adjust

--- returnPermutations.js
+++ returnPermutations.js
@@ -1,19 +1,19 @@
 //returns all possible combinations of a string recursively
 
 let findCombos = (str) => {
-    let combos = [];
+  let combos = [];
 
-    if (str.length < 2) {
-      return str;
-    }
-
-    for (let i = 0; i < str.length; i++) {
-      let char = str[i];
-      let remainingChars = str.slice(0, i) + str.slice(i + 1, str.length);
-
-      for (let combo of findCombos(remainingChars)) {
-        combos.push(char + combo)
-      }
-    }
-    return combos;
+  if (str.length < 2) {
+    return str;
   }
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    let remainingChars = str.slice(0, i) + str.slice(i + 1, str.length);
+
+    for (let combo of findCombos(remainingChars)) {
+      combos.push(char + combo)
+    }
+  }
+  return combos;
+}
